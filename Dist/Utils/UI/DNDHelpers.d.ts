a8ecdee60602fac005631c32cdd222d558521099
Venryx <venryx@gmail.com>
2019-12-28 09:15:22 -0800

    * Fix that drag-and-drop breaks after first dnd (usually).

--- Dist/Utils/UI/DNDHelpers.d.ts
+++ Dist/Utils/UI/DNDHelpers.d.ts
@@ -1,10 +1,33 @@
-import { DraggableInfo } from "./DNDStructures";
-export declare type DragInfo = {
-    provided: any;
-    snapshot: any;
-};
-export declare function MakeDraggable(getDraggableCompProps: (props: Object) => {
+import React from "react";
+declare type DraggableCompProps = {
     type: string;
     draggableInfo: DraggableInfo;
     index: number;
-}): (WrappedComponent: any) => any;
+};
+export declare type DropProvided = {
+    innerRef: (element: HTMLElement) => any;
+    placeholder?: React.ReactElement<any>;
+    droppableProps: any;
+};
+export declare type DropSnapshot = {
+    isDraggingOver: boolean;
+    draggingOverWith?: string;
+};
+export declare type DropInfo = {
+    provided: DropProvided;
+    snapshot: DropSnapshot;
+};
+export declare type DragProvided = {
+    draggableProps: any;
+    dragHandleProps: any;
+};
+export declare type DragSnapshot = {
+    isDragging: boolean;
+};
+export declare type DragInfo = {
+    provided: DragProvided;
+    snapshot: DragSnapshot;
+};
+declare type DraggableInfo = any;
+export declare function MakeDraggable(getDraggableCompProps: (props: Object) => DraggableCompProps): (WrappedComponent: any) => any;
+export {};
